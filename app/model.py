5cfcb688ee64f883247517170be854a650dee026
ZBOOK <i233074@isb.nu.edu.pk>
2025-10-17 01:05:01 +0500

    Completed backend

--- app/model.py
+++ app/model.py
@@ -1,16 +1,9 @@
 import numpy as np
 
-def predict(features: list, model, label_encoder):
-    # Convert input to array
-    input_data = np.array(features).reshape(1, 20, 22)  # adjust shape as needed
-
-    # Get prediction probabilities
+def predict(model, label_encoder, features: list):
+    input_data = np.array(features).reshape(1, 20, 22)
     preds = model.predict(input_data)
     class_index = np.argmax(preds, axis=1)[0]
     confidence = float(np.max(preds))
-
-    # Decode label
     gesture = label_encoder.inverse_transform([class_index])[0]
-    print(gesture)
     return gesture, confidence
-
